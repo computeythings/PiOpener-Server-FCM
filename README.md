50bc21eacdf53fcf3ed49eb7deec8c097078d82b
computeythings <25091077+computeythings@users.noreply.github.com>
2018-03-26 08:58:46 -0700

    Begin conversion of https://github.com/computeythings/PiOpener-Server.git to a node.js base for FCM integration.

---
+++ README.md
@@ -0,0 +1,58 @@
+# RPi Garage Opener
+A server script to handle requests to control and monitor garage door state.
+
+## Requirements
+This script requires a Raspberry Pi with SSH enabled as well as an available 3 GPIO pins.
+
+Other than that you'll just need a 5V relay with a low level amp allowing it to be controlled by the 3.3V GPIO pins.
+
+## Wiring
+The pigpio npm package uses the Broadcom SOC channel (BCM) numbering scheme
+
+![here](https://www.jameco.com/Jameco/workshop/circuitnotes/raspberry_pi_circuit_note_fig2a.jpg)
+
+That would be the values on the end of the "GPIO#" labels on the respective pins in the image above.
+
+You should be connecting your relay to a 5V and Ground pin on the pi for power as well a GPIO pin. This is your RELAY_PIN.
+
+Your garage door should also have two switches on the rails. One next to the opener and one on the further end by the actual opening of the garage. Run a wire to each of these wiring them to separate GPIO pins. The closer switch will be your OPEN switch while the further your CLOSE switch.
+
+Once you've connected those, connect the relay to the two terminals that your physical wall switch is connected to. When the script is told to toggle the door it will quickly close and open the switch simulating a button press. Find a good place to mount your pi and then plug it in.
+
+## Installation
+To install, simply clone the directory, and run the setup script as root:
+
+    $ git clone https://github.com/computeythings/PiOpener-Server.git
+    $ cd PiOpener-Server
+    $ sudo python3 ./setup.py
+
+You should then be prompted to enter the pin to which the relay is connected as well as the input pins which the open/closed switches are connected.
+
+Following this you will be prompted to fill out info for your SSL cert.
+
+**Important:** If you plan on using the Android app and wish to control your garage opener from outside of your home network, you must fill out the Common Name with your serves **external** IP address when prompted during creation of the SSL cert. It's highly recommended you use a dynamic DNS name. This is important when performing hostname verification and will cause the app to refuse the connection externally if it is not setup properly. The rest of the information can be filled out with whatever you'd like as it probably won't ever be relevant again.
+
+Once you've finished this, the script should migrate all files to `/opt` and setup, enable, and start a systemd service called garageopener.service. To check if this has worked, run `systemctl status garageopener.service` on the pi. You should get some info on the service including some green text saying "active (running)".
+
+That's it! Your garage opener is now up and running and ready to receive some commands.
+
+## REST API
+To control your garage door from other devices you must `POST` requests to `https://<raspberrypi.ip>/api/` and include the API key in the POST data as "access_token" as well as either `OPEN`, `CLOSE`, `TOGGLE`, or `QUERY` as your "intent".
+
+An example would be a file payload.json
+
+    {
+      "access_token": "<your-key-here>",
+      "intent": "OPEN"
+    }
+
+Followed by this command: `curl -kd @payload.json https://<raspberrypi.ip>:4443/api/`
+
+This should be followed by a response of "Opening garage" and the actual opening of your garage if it is closed or nothing if it is already open.
+
+The `TOGGLE` intent will trigger the garage door regardless of its current position.
+
+`QUERY` is the only intent that will not trigger an action. It simply responds with `OPEN`, `CLOSED`, or `NEITHER` if it is somewhere in between. This is useful, for example, if all you want is to make sure your garage is closed after you leave.
+
+## TCP server
+The TCP server opens a port on the server to accept a socket connection which will allow you to monitor and get real-time updates of the garage door status. This is intended to be used with the [PiOpener-Android app](https://github.com/computeythings/PiOpener-Android).
---
+++ src/gopener.js
@@ -0,0 +1,191 @@
+"use strict"
+const GPIO = require('pigpio').Gpio;
+const sleep = require('sleep');
+
+const OPEN = 'OPEN';
+const CLOSED = 'CLOSED';
+
+class Opener {
+  constructor(openPin, closedPin, relayPin) {
+    // socketClients will be alerted on state changes
+    this.socketClients = [];
+    // Setup GPIO pins
+    this.openPin = new GPIO(openPin,
+      {
+        mode: GPIO.INPUT,
+        pullUpDown: GPIO.PUD_UP,
+        edge: GPIO.EITHER_EDGE
+      }
+    );
+
+    this.closedPin = new GPIO(closedPin,
+      {
+        mode: GPIO.INPUT,
+        pullUpDown: GPIO.PUD_UP,
+        edge: GPIO.EITHER_EDGE
+      }
+    );
+    this.relayPin = new GPIO(relayPin, {mode: GPIO.OUTPUT});
+
+    this.lastTriggered = 0; // Debounce variable
+    this.openPin.on('interrupt', (level) => {
+      var now = Date.now();
+      if (now - this.lastTriggered > 300) {
+        this.openTrigger(level);
+        this.lastTriggered = now;
+      }
+    });
+    this.closedPin.on('interrupt', (level) => {
+      var now = Date.now();
+      if (now - this.lastTriggered > 300) {
+        this.closeTrigger(level);
+        this.lastTriggered = now;
+      }
+    });
+
+    this.isFullyOpen = !this.openPin.digitalRead();
+    this.isFullyClosed = !this.closedPin.digitalRead();
+    this.isOpening = false;
+    this.isClosing = false;
+    this.want = 0; // Changed per user request
+
+
+    this.openTrigger = function(level) {
+      if (level)
+        this.closing();
+      else
+        this.opened();
+    }
+    this.closeTrigger = function(level) {
+      if (level)
+        this.opening();
+      else
+        this.closed();
+    }
+
+    /**
+      Since there's no open/close wire to connect to, we can only toggle the door.
+      A software check is run to determine whether or not we should toggle
+      given each command.
+
+      If the garage is in a middle state (i.e. not fully open OR closed) when the
+      command is run, the door will be toggled once and then again if the desired
+      state was not reached.
+    **/
+
+    /* Quickly toggle a relay closed and open to simulate a button press */
+    this.toggle = function() {
+      this.relayPin.trigger(200000, 0);
+    }
+
+    this.toggleGarage = function() {
+      if (this.isOpening || this.isFullyOpen)
+        this.closeGarage();
+      else if (this.isClosing || this.isFullyClosed)
+        this.openGarage();
+      else {
+        console.log('Status unverifiable: toggling garage.')
+        this.toggle();
+      }
+    }
+
+    this.openGarage = function() {
+      console.log('Opening garage');
+      if (!this.isFullyOpen) {
+        this.want = OPEN;
+        this.toggle();
+        this.updateClient();
+      } else
+        console.log('Garage is already open.');
+    }
+
+    this.closeGarage = function() {
+      console.log('Closing garage');
+      if (!this.isFullyClosed) {
+        this.want = CLOSE;
+        this.toggle();
+        this.updateClient();
+      } else
+        console.log('Garage is already closed.');
+    }
+
+    /* This is run when the openPin switch is connected */
+    this.opened = function() {
+      console.log('Garage is now open.');
+      this.isFullyOpen = true;
+      this.isOpening = false;
+      if (this.want == CLOSED) // Toggle again if the intent was to close
+        this.closeGarage();
+      else
+        this.want = 'NONE';
+      this.updateClient();
+    }
+
+    /* This is run when the closedPin switch is disconnected */
+    this.closing = function() {
+      console.log('Garage is no longer open.');
+      this.isClosing = true;
+      this.isFullyOpen = false;
+      this.updateClient();
+    }
+
+    /* This is run when the closedPin switch is connected */
+    this.closed = function() {
+      console.log('Garage is now closed.');
+      this.isFullyClosed = true;
+      this.isClosing = false;
+      if (this.want == OPEN) // Toggle again if the intent was to open
+        this.openGarage();
+      else
+        this.want = 'NONE';
+      this.updateClient();
+    }
+
+    /* This is run when the openPin switch is disconnected */
+    this.opening = function() {
+      console.log('Garage is no longer closed.');
+      this.isOpening = true;
+      this.isFullyClosed = false;
+      this.updateClient();
+    }
+
+    /* Returns status info */
+    this.status = function() {
+      var data = {};
+      data.OPEN = this.isFullyOpen;
+      data.CLOSED = this.isFullyClosed;
+      data.OPENING = this.isOpening;
+      data.CLOSING = this.isClosing;
+      return data;
+    }
+
+    /* Add a client to be updated on state changes */
+    this.addClient = function(client) {
+      this.socketClients.push(client);
+    }
+
+    /* Remove from client update list */
+    this.removeClient = function(client) {
+      this.socketClients.splice(socketClients.indexOf(client), 1);
+    }
+
+    /*
+      This is run on any state change and will return data to a socket
+      client that has implemented an update callback method.
+    */
+    this.updateClient = function() {
+      this.socketClients.forEach((client) => {
+        element.update(this.status());
+      });
+    }
+  }
+
+  get isOpen() {
+    return this.isFullyOpen;
+  }
+  get isClosed() {
+    return this.isFullyClosed;
+  }
+}
+
+const opener = new Opener(2,3,4);
