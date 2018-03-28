813053f2f503fabeb7c95ea063df840ce083c355
computeythings <25091077+computeythings@users.noreply.github.com>
2018-03-28 15:17:47 -0700

    Client data processing

--- src/sockserver.js
+++ src/sockserver.js
@@ -3,7 +3,12 @@ const tls = require('tls');
 const fs = require('fs');
 
 const DEFAULT_PORT = 4443; // Default listening port
-const HOST_ADDR = '127.0.0.1';
+// Potential command values received from clients
+const KILL_COMMAND = 'KILL';
+const REFRESH_COMMAND = 'REFRESH';
+const OPEN_COMMAND = 'OPEN_GARAGE';
+const CLOSE_COMMAND = 'CLOSE_GARAGE';
+const TOGGLE_COMMAND = 'TOGGLE_GARAGE';
 
 module.exports = class TCPServer {
   constructor(opener, port, apikey, cert, key, logf) {
@@ -14,53 +19,58 @@ module.exports = class TCPServer {
       this.key = fs.readFileSync(key);
       this.logf = logf;
 
-      this.connectionListener = function(sock) {
-        console.log('TCP server started at ' + sock.remoteAddress + ':' +
-                      sock.remotePort);
-        sock.on('data', (data) => {
-          console.log('received: ' + data);
+      this.listener = function(socket) {
+        console.log('client accepted at address ' + socket.remoteAddress);
+        // Clients are able to send data over the socketet to control this.opener
+        socket.on('data', (data) => {
+          // Process data sent by client
+          switch(data) {
+            case KILL_COMMAND: // Kill the connection
+              console.log('socketet close requested');
+              socket.end();
+              break;
+            case REFRESH_COMMAND: // Client requests updated opener status
+              console.log('Client refresh');
+              this.opener.updateClient();
+              break;
+            case OPEN_COMMAND: // Client requests to open opener
+              console.log('Opening garage');
+              this.opener.openGarage();
+              break;
+            case CLOSE_COMMAND: // Client requests to close opener
+              console.log('Closing garage');
+              this.opener.closeGarage();
+              break;
+            case TOGGLE_COMMAND: //Client requests to toggle opener
+              console.log('Toggling garage');
+              this.opener.toggleGarage();
+              break;
+            default:
+              console.log('Client sent: ' + data);
+          }
         });
-        sock.on('close', (data) => {
+        socket.on('close', (data) => {
           console.log('TCP server closed');
         });
       }
 
       if(this.cert && this.key) {
-        console.log('starting SSL socket server');
+        console.log('starting SSL socketet server');
         this.credentials = {key: this.key, cert: this.cert};
-        this.server = tls.createServer(this.credentials, (sock) => {
-          console.log('TCP server started at ' + sock.remoteAddress + ':' +
-                        sock.remotePort);
-          sock.on('data', (data) => {
-            console.log('received: ' + data);
-          });
-          sock.on('close', (data) => {
-            console.log('TCP server closed');
-          });
-        });
+        this.server = tls.createServer(this.credentials, this.listener);
       } else {
-        this.server = tls.createServer((sock) => {
-          console.log('TCP server started at ' + sock.remoteAddress + ':' +
-                        sock.remotePort);
-          sock.on('data', (data) => {
-            console.log('received: ' + data);
-          });
-          sock.on('close', (data) => {
-            console.log('TCP server closed');
-          });
-        });
+        this.server = tls.createServer(this.listener);
       }
 
   }
 
   start() {
-    this.server.listen(this.port || DEFAULT_PORT, () => {
-      console.log('server bound');
+    this.server.listen(this.port||DEFAULT_PORT, () => {
+      console.log('TCP server started on port: ' + this.port||DEFAULT_PORT);
     });
     this.server.on('connection', (conn) => {
         console.log('client accepted at address: ' + conn.remoteAddress + ':' +
                       conn.remotePort);
     });
-    console.log('eof');
   }
 }
