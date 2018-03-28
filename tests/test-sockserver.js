8567f7fb103d235a7c99151315acb0600a982d1a
computeythings <25091077+computeythings@users.noreply.github.com>
2018-03-28 15:01:54 -0700

    Rearranged parameters to match modules. Added sockserver test

--- tests/test-sockserver.js
+++ tests/test-sockserver.js
@@ -1,9 +1,9 @@
-const RESTServer = require('../src/restserver.js');
+const TCPServer = require('../src/sockserver.js');
 const Opener = require('../src/gopener.js');
 
 var certLocation = '/etc/ssl/certs/garageopener.pem';
 var keyLocation = '/etc/ssl/private/garageopener.key';
 
-const server = new RESTServer(new Opener(2,3,4), 4453, 'aoeu',
+const server = new TCPServer(new Opener(2,3,4), 4453, 'aoeu',
                                 certLocation, keyLocation, 'aoeu');
 server.start();
